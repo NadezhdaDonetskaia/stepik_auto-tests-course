8b33e9d84effc21352920753cb352e3f48b63dc5
Nella <nella611@ya.ru>
2020-12-18 19:24:26 +0200

    new test leng

--- launching autotests for different interface languages/test_items.py
+++ launching autotests for different interface languages/test_items.py
@@ -4,5 +4,5 @@ def test_button_to_be(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element_by_css_selector('button.btn-primary')
-    #time.sleep(20)
+    #time.sleep(20)    
     assert button.is_displayed() == True, "Кнопки добавить в корзину нет на экране"
