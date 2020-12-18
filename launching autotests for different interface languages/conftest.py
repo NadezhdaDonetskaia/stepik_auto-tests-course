e22a09352c89eee72562d6b4e81d5a01f67acfb7
Nella <nella611@ya.ru>
2020-12-18 19:13:42 +0200

    finish version

--- launching autotests for different interface languages/conftest.py
+++ launching autotests for different interface languages/conftest.py
@@ -28,9 +28,3 @@ def browser(request):
     yield browser
     print("\nquit browser...")
     browser.quit()
-
-
-
-
-
-    
--- launching autotests for different interface languages/test_items.py
+++ launching autotests for different interface languages/test_items.py
@@ -1,6 +1,8 @@
+import time
 
 def test_button_to_be(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button = chrome.find_element_by_css_selector('button.btn-primary')
+    button = browser.find_element_by_css_selector('button.btn-primary')
+    #time.sleep(20)
     assert button.is_displayed() == True, "Кнопки добавить в корзину нет на экране"
